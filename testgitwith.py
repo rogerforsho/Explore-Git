406167ec98b1ca475eecb03a3c653b3855c562c3
rogerforsho <your_email@example.com>
2025-08-22 10:00:33 +0800

    change?

---
+++ testgitwith.py
@@ -0,0 +1,15 @@
+# Simple Python Program
+
+# Print a message
+print("Hello, welcome to Python!")
+
+# Ask for user's name
+name = input("Enter your name: ")
+print("Nice to meet you,", name)
+
+# Simple math
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+sum_result = num1 + num2
+print("The sum is:", sum_result)
